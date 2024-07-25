ad50ecac2ead799057abb4bd2b044679b29458c3
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-07-25 11:41:47 +0100

    drape users

--- drape_api/DrapeSite/users/admin.py
+++ drape_api/DrapeSite/users/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from users.models import User
+
 from django.utils.translation import gettext as _
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'username', 'first_name', 'last_name', 'date_joined']
+    search_fields = ['username', 'email']
+    list_filter = ['date_joined']
