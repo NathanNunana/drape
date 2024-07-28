7124f861eda6a647d82e5a18c21a2d7f02d06e08
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-07-28 22:41:58 +0200

    UI

--- drape_api/DrapeSite/DrapeSite/settings.py
+++ drape_api/DrapeSite/DrapeSite/settings.py
@@ -71,10 +71,10 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000', 
-]
-# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000', 
+# ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 
