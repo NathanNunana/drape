ae11b95a45246ff2b22f99167ae3031162c96611
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-08-18 17:53:34 +0200

    Celery

--- drape_api/DrapeSite/DrapeSite/settings.py
+++ drape_api/DrapeSite/DrapeSite/settings.py
@@ -136,13 +136,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
+# Ensure these settings match your timezone configuration
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Africa/Accra'
-
 USE_I18N = True
-
 USE_TZ = True
+
+# Celery timezone settings
+CELERY_TIMEZONE = 'Africa/Accra'
+CELERY_ENABLE_UTC = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -204,6 +206,6 @@ CELERY_TIMEZONE = 'UTC'
 CELERY_BEAT_SCHEDULE = {
     'send-reminder-email': {
         'task': 'drape_app.tasks.schedule_reminders',
-        'schedule': crontab(hour=7, minute=0),  # Runs daily at midnight UTC
+        'schedule': crontab(hour=16, minute=0),  # Runs daily at 4 PM Accra time
     },
 }
